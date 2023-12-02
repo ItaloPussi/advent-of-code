bcda36b237e7af7a779355e09f09c0aec1cbcb2b
ItaloPussi <italopussi@gmail.com>
2023-12-02 14:44:30 -0300

    Advent of Code 2023: Day 02

---
+++ 2023/day02/main.py
@@ -0,0 +1,109 @@
+"Advent of Cyber 2023 - Day 02 - Exercises 01 and 02"
+from typing import List, Literal
+import re
+
+
+def extract_colors_counts(game: str, ball_color: Literal['red', "green", "blue"]) -> List[int]:
+    """
+        Given a game and a color, extract the amount of balls of the color
+
+        Parameters:
+        - game (str): The text representation of the game.
+        - ball_color (Literal['red', "green", "blue"]): The ball color.
+
+        Returns:
+        - List[int]: A list containing the amount of balls of that color
+    """
+    return [int(k) for k in re.findall(rf"(\d+) {ball_color}", game)]
+
+
+def is_game_possible(game: str, max_rgb_balls: List[int]) -> bool:
+    """
+        Validate if a game is valid given the max amount of RGB balls.
+
+        Parameters:
+        - game (str): The text representation of the game.
+        - max_rgb_balls (List[int]): The max allowed number of red, green and blue balls.
+
+        Returns:
+        - bool: True if the game is possible, False otherwise.
+    """
+
+    max_red_balls, max_green_balls, max_blue_balls = max_rgb_balls
+
+    red_balls = extract_colors_counts(game, "red")
+    is_red_valid = max_red_balls >= max(red_balls)
+
+    green_balls = extract_colors_counts(game, "green")
+    is_green_valid = max_green_balls >= max(green_balls)
+
+    blue_balls = extract_colors_counts(game, "blue")
+    is_blue_valid = max_blue_balls >= max(blue_balls)
+
+    return is_red_valid and is_blue_valid and is_green_valid
+
+
+def get_possible_games_sum(games: List[str]) -> int:
+    """
+    Given a list of games, calculates the sum of the IDs of all the valid ones.
+
+    Parameters:
+    - games (List[str): A list of games.
+
+    Returns:
+    - int: The sum of the IDs of all the valid games.
+    """
+    output_sum = 0
+    for game in games:
+        game_possible = is_game_possible(game, [12, 13, 14])
+        if game_possible:
+            game_id = re.search(r"Game (\d+)", game).group(1)
+            output_sum += int(game_id)
+    return output_sum
+
+
+def get_game_balls_min_amount(game: str) -> int:
+    """
+        Given a game, the multiplication of the minimum amount of balls for the game be valid.
+
+        Parameters:
+        - game (str): The text representation of the game.
+
+        Returns:
+        - int: The multiplication of the RGB amount
+    """
+
+    red_amount = max(extract_colors_counts(game, "red"))
+    green_amount = max(extract_colors_counts(game, "green"))
+    blue_amount = max(extract_colors_counts(game, "blue"))
+
+    return red_amount * green_amount * blue_amount
+
+
+def get_game_balls_min_amount_sum(games: str) -> int:
+    """
+        Given a list of games, the sum of the result of the function 
+        get_game_balls_min_amount for each game.
+
+        Parameters:
+        - games (List[str): A list of games.
+
+        Returns:
+        - int: The sum of the result of the function get_game_balls_min_amount for each game.
+    """
+
+    output_sum = 0
+    for game in games:
+        game_power = get_game_balls_min_amount(game)
+        output_sum += game_power
+    return output_sum
+
+
+if __name__ == "__main__":
+    with open("./input.txt", encoding="utf-8") as input_file:
+        input_items = input_file.readlines()
+        print("===== Cube Conundrum (Day 02) - Advent of Code 2023  =====")
+        print(
+            f"The first star result is {get_possible_games_sum(input_items)}")
+        print(
+            f"The second star result is {get_game_balls_min_amount_sum(input_items)}")
---
+++ 2023/day02/readme.md
@@ -0,0 +1,41 @@
+# Cube Conundrum (Day 02) - Advent of Code 2023 
+
+## The First Star
+Process a document consisting of lines of text. Each line contains a textual representation of a game, and the goal is to determine if each game is valid. A game is valid if during the rounds, the number of balls of a given color doesn't exceed the maximum allowed value for that color. The ultimate objective is to calculate the sum of all the IDs of the valid games.
+
+- The ID of the game is the number before the colon(:)
+- The rounds starts after the colon and are divided by a semicolon(;)
+
+Example:
+```
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+ID: 1
+Round 1: 3 blue, 4 red; 1 red
+Round 2: 1 red, 2 green, 6 blue
+Round 3: 2 green
+
+If the maximum amount of RGB balls are 6, 6, 5 the game would not be valid because in round 2 we have 6 blue balls (and the maximum is 5).
+If the maximum amount of RGB balls are 6, 6, 6 the game would be valid because none of the balls exceed the maximum  allowed amount
+```
+
+## The Second Star
+Improve the created code to instead of validating a game, calculate the multiplication of the minimum amounts of RGB balls necessary to game be valid.
+Return the sum of the value of all the games.
+
+Example:
+```
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+
+The minimum amount of RGB are 4, 6, 2 and the result would be 4 * 6 * 2 = 48.
+
+```
+
+## Running the code
+To run the code, just use the following command in this directory in a environment with Python3 installed and it will give you the result:
+```
+    python3 main.py
+```
+
+## About
+Advent of Code is an annual programming event created by the [Advent of Code](https://adventofcode.com) enterprise where you've to collect 50 stars by completing daily programmings challenges. You can learn more about this challenge [here](https://adventofcode.com/2023/day/2).
