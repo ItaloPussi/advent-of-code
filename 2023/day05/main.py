950568a69f153f1852656508a392b5631a7b7d40
ItaloPussi <italopussi@gmail.com>
2023-12-05 21:04:09 -0300

    Advent of Code 2023: Day 05

---
+++ 2023/day05/main.py
@@ -0,0 +1,125 @@
+"Advent of Cyber 2023 - Day 05 - Exercises 01 and 02"
+from typing import List, Tuple
+from itertools import groupby
+
+
+def get_seed_location(seed: int, almanac_maps: List[str]) -> Tuple[int, List[int]]:
+    """
+    Returns location and the  followed path of a given seed in the almanac maps.
+
+    Parameters:
+    - seed (int): The seed to search for.
+    - almanac_maps (List[str]): List of almanac maps.
+
+    Returns:
+    Tuple[int, List[int]]: A tuple containing the final location of the seed and the path taken.
+    """
+
+    path = []
+    searched_number = seed
+    for almanac_map in almanac_maps[1:]:
+        for map_line_index, map_line in enumerate(almanac_map[1:]):
+            destination, seed_source, seed_source_amount = [
+                int(k) for k in map_line.split()]
+
+            if (searched_number >= seed_source and searched_number <= (seed_source + seed_source_amount)):
+                searched_number = destination + (searched_number - seed_source)
+                path.append(map_line_index)
+                break
+    return searched_number, path
+
+
+def get_minimum_seed_location(almanac: List[str]) -> int:
+    """
+    Finds the minimum location of a seed in the almanac.
+
+    Parameters:
+    - almanac (List[str]): List of almanac maps.
+
+    Returns:
+    int: The minimum location of a seed in the almanac.
+    """
+
+    almanac_maps = [list(g)
+                    for k, g in groupby(almanac, key=lambda x: x != "") if k]
+
+    seeds = [int(k) for k in almanac_maps[0][0].split(": ")[1].split()]
+    seeds_locations = [get_seed_location(seed, almanac_maps)[
+        0] for seed in seeds]
+    return min(seeds_locations)
+
+
+def range_get_seed_location(range_start, range_end, almanac_parts):
+    """
+    Finds the seed location in the given range of the almanac.
+
+    Parameters:
+    - range_start (int): The start of the range.
+    - range_end (int): The end of the range.
+    - almanac_parts (List[str]): List of almanac maps.
+
+    Returns:
+    None
+    """
+
+    stack = [(range_start, range_end)]
+
+    while stack:
+        current_range_start, current_range_end = stack.pop()
+
+        if current_range_start > current_range_end:
+            continue
+
+        current_range_start_location, current_range_start_path = get_seed_location(
+            current_range_start, almanac_parts)
+        _, current_range_end_path = get_seed_location(
+            current_range_end, almanac_parts)
+
+        # Here is the charm: If both ends of the range shares a same path, it means the in between numbers shares the path too
+        if current_range_start_path == current_range_end_path:
+            global second_star_min_number
+            if current_range_start_location > 0 and (second_star_min_number is None or current_range_start_location < second_star_min_number):
+                second_star_min_number = current_range_start_location
+            continue
+
+        mid = (current_range_start + current_range_end) // 2
+        stack.append((current_range_start, mid - 1))
+        stack.append((mid, current_range_end + 1))
+
+
+second_star_min_number = None
+
+
+def range_get_seed_min_location(almanac: list[str]) -> int:
+    """
+    Finds the minimum location of seeds in the almanac within specified ranges.
+
+    Parameters:
+    - almanac (List[str]): List of almanac maps.
+
+    Returns:
+    int: The minimum location of seeds in the almanac within specified ranges.
+    """
+
+    almanac_maps = [list(g)
+                    for k, g in groupby(almanac, key=lambda x: x != "") if k]
+
+    seeds = [int(k) for k in almanac_maps[0][0].split(": ")[1].split()]
+
+    for index, seed in enumerate(seeds):
+        if (index % 2 == 0):
+            range_get_seed_location(seed, seed + seeds[index+1], almanac_maps)
+        else:
+            continue
+
+    return second_star_min_number
+
+
+if __name__ == "__main__":
+    with open("./input.txt", encoding="utf-8") as input_file:
+        input_items = [k.strip() for k in input_file.readlines()]
+        print("===== If You Give A Seed A Fertilizer (Day 05) - Advent of Code 2023  =====")
+        print(
+            f"The first star result is {get_minimum_seed_location(input_items)}")
+        print(
+            f"The second star result is {range_get_seed_min_location(input_items)}")
---
+++ 2023/day05/readme.md
@@ -0,0 +1,17 @@
+# If You Give A Seed A Fertilizer (Day 05) - Advent of Code 2023 
+
+## The First Star
+A Given almanac (your puzzle input) lists all of the seeds that need to be planted. It also lists what type of soil to use with each kind of seed, what type of fertilizer to use with each kind of soil, what type of water to use with each kind of fertilizer, and so on. Every type of seed, soil, fertilizer and so on is identified with a number, but numbers are reused by each category - that is, soil 123 and fertilizer 123 aren't necessarily related to each other.
+Mapping a seed by each almanac part will result in a location. The challenge is to get the minimum location based in all the seeds.
+
+## The Second Star
+Now, treat the the values on the initial seeds as pairs. Within each pair, the first value is the start of the range and the second value is the length of the range.
+
+## Running the code
+To run the code, just use the following command in this directory in a environment with Python3 installed and it will give you the result:
+```
+    python3 main.py
+```
+
+## About
+Advent of Code is an annual programming event created by the [Advent of Code](https://adventofcode.com) enterprise where you've to collect 50 stars by completing daily programmings challenges. You can learn more about this challenge [here](https://adventofcode.com/2023/day/5).
