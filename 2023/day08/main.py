72988853088bcc6c5d33bcb5fcc179f5b4bc82c7
ItaloPussi <italopussi@gmail.com>
2023-12-08 12:41:09 -0300

    Advent of Code 2023: Day 08

---
+++ 2023/day08/main.py
@@ -0,0 +1,114 @@
+"Advent of Cyber 2023 - Day 08 - Exercises 01 and 02"
+from typing import Tuple, List, Dict
+import re
+from math import lcm
+
+
+def convert_map_to_dict(map_list: list[str]) -> Dict[str, Tuple[str, str]]:
+    """
+    Converts a map represented as a list of strings into a dictionary where each
+    key is a starting position and the corresponding value is a tuple of left and
+    right positions.
+
+    Parameters:
+    - map_list (list[str]): A list of strings representing the map.
+
+    Returns:
+    - Dict[str, Tuple[str, str]]: A dictionary mapping starting positions to
+      tuples of left and right positions.
+    """
+
+    dict_map = {}
+
+    for line in map_list:
+        start, left, right = re.findall(r"(\w{3})", line)
+        dict_map[start] = (left, right)
+
+    return dict_map
+
+
+def get_single_total_steps(position: str, allowed_objectives: List[str], dict_map: Dict[str, Tuple[str, str]], directions: List[str]) -> int:
+    """
+    Calculates the number of steps needed to reach any of the allowed objectives
+    from a given starting position, following the specified directions.
+
+    Parameters:
+    - position (str): The starting position.
+    - allowed_objectives (List[str]): List of allowed objective positions.
+    - dict_map (Dict[str, Tuple[str, str]]): A dictionary mapping starting
+      positions to tuples of left and right positions.
+    - directions (List[str]): List of directions to follow ("L" or "R").
+
+    Returns:
+    - int: The total number of steps needed.
+    """
+
+    steps = 0
+    current_position = position
+    current_direction_index = 0
+
+    while current_position not in allowed_objectives:
+        steps += 1
+
+        direction_index = 0
+        if directions[current_direction_index] == "R":
+            direction_index = 1
+
+        current_position = dict_map[current_position][direction_index]
+
+        current_direction_index = current_direction_index + \
+            1 if current_direction_index < len(directions) - 1 else 0
+
+    return steps
+
+
+def get_person_total_steps(map_list: list[str]) -> int:
+    """
+    Calculates the total number of steps needed for a person to reach the
+    objective on the provided map.
+
+    Parameters:
+    - map_list (list[str]): A list of strings representing the map.
+
+    Returns:
+    - int: The total number of steps needed for a person to reach the objective.
+    """
+
+    directions = list(map_list[0])
+    dict_map = convert_map_to_dict(map_list[2:])
+
+    return get_single_total_steps("AAA", "ZZZ", dict_map, directions)
+
+
+def get_ghost_total_steps(map_list: list[str]) -> int:
+    """
+    Calculates the total number of steps needed for a ghost to reach the
+    objective on the provided map, considering multiple entry points.
+
+    Parameters:
+    - map_list (list[str]): A list of strings representing the map.
+
+    Returns:
+    - int: The total number of steps needed for a ghost to reach the objective.
+    """
+
+    directions = list(map_list[0])
+    dict_map = convert_map_to_dict(map_list[2:])
+
+    entry_points = list(filter(lambda x: x[-1] == "A", dict_map.keys()))
+    objectives = list(filter(lambda x: x[-1] == "Z", dict_map.keys()))
+
+    steps = [get_single_total_steps(
+        k, objectives, dict_map, directions) for k in entry_points]
+
+    return lcm(*steps)
+
+
+if __name__ == "__main__":
+    with open("./input.txt", encoding="utf-8") as input_file:
+        input_items = [k.strip() for k in input_file.readlines()]
+        print("===== Haunted Wasteland  (Day 08) - Advent of Code 2023  =====")
+        print(
+            f"The first star result is {get_person_total_steps(input_items)}")
+        print(
+            f"The second star result is {get_ghost_total_steps(input_items)}")
---
+++ 2023/day08/readme.md
@@ -0,0 +1,51 @@
+# Camel Cards (Day 06) - Advent of Code 2023 
+
+## The First Star
+A Given document contains a list of left/right instructions, and a network of labeled nodes.
+Starting from node AAA, get the total steps required to get to ZZZ.
+If there are no more directions, cycle by the directions again.
+
+Example:
+```
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+
+Starting in AAA, we should go right to CCC. From there, left to ZZZ. 2 steps in total.
+```
+
+## The Second Star
+Now, consider as entrypoints all the nodes who ends with "A". The goal is to get the total steps required to all the entrypoints make to the goal. If a entrypoint is in the goal, but the others are not, it should continue the cycle until all them match.
+
+Example:
+```
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+
+Entrypoints = 11A and 22A
+Goal = 11Z and 22Z
+Total steps = 6
+```
+
+## Running the code
+To run the code, just use the following command in this directory in a environment with Python3 installed and it will give you the result:
+```
+    python3 main.py
+```
+
+## About
+Advent of Code is an annual programming event created by the [Advent of Code](https://adventofcode.com) enterprise where you've to collect 50 stars by completing daily programmings challenges. You can learn more about this challenge [here](https://adventofcode.com/2023/day/8).
